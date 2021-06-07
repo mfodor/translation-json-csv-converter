35d3c4d9353072ad5ba81e403f1fe9f1c4f86af3
mfodor <manuel.fodor@itware.hu>
2021-06-07 14:06:51 +0200

    bugfix

--- app/parseParams.js
+++ app/parseParams.js
@@ -99,8 +99,8 @@ function setSource(source) {
  * @param {string} target
  */
 function setTarget(target) {
-    if (!target || !fs.existsSync(target)) {
-        console.log('Target not provided or not exists: %s!', target);
+    if (!target) {
+        console.log('Target not provided: %s!', target);
         process.exit(2);
     }
     params.target = target;
@@ -118,12 +118,8 @@ function setDelimiter(delimiter) {
 }
 
 function validate() {
-    if (!fs.existsSync(params.source)) {
-        console.log('Source does not exist!')
-        process.exit(4);
-    }
     if (params.direction === JSON_TO_CSV) {
-        if (!params.source || !fs.lstatSync(params.source).isDirectory()) {
+        if (!params.source || !fs.existsSync(params.source) || !fs.lstatSync(params.source).isDirectory()) {
             console.log('Source must be a directory containing the translation JSON files!');
             process.exit(4);
         }
